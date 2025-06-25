599ce2a3c456257917a9ff67a4209d70619efdcb
shadman-a <ahmedshadman966@gmail.com>
2025-06-25 13:38:33 -0400

    Initial Commit

---
+++ CoverFlow/CoverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowApp.swift
+//  CoverFlow
+//
+//  Created by Shadman Ahmed on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
